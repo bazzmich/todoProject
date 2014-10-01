535a5e27c7b862b5f49361c94ef833038e9846f3
bazzmich <bazzermeister@gmail.com>
2014-10-01 14:29:10 +0300

    change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 angular.module('toDo', []).
 controller('toDoCtrl', ['$scope', function ($scope) {
 	$scope.todos = [];
+	$scope.markAll = false;
 	$scope.saved = localStorage.getItem('todos');
 	$scope.todos = (localStorage.getItem('todos')!==null) ? JSON.parse($scope.saved) : [];
 	localStorage.setItem('todos', JSON.stringify($scope.todos));
@@ -9,14 +10,26 @@ controller('toDoCtrl', ['$scope', function ($scope) {
     	return $scope.todos.length;
   	}
 
+	$scope.toggleMarkAll = function() {
+      angular.forEach($scope.todos, function(todo) {
+        todo.done = $scope.markAll;
+      });
+	};
+
 	$scope.addTask = function () {
 		$scope.todos.push({
 			'title': $scope.newTask,
-			'done': false
+			'done': false,
+			'editing': false
 		});
 		$scope.newTask = '';
 		localStorage.setItem('todos', JSON.stringify($scope.todos));
 	}
+
+	$scope.isTodo = function(){
+      return $scope.todos.length > 0;  
+	}
+
 	$scope.remainingTasks = function() {
 		var count = 0;
 		angular.forEach($scope.todos, function(todo){
@@ -24,12 +37,36 @@ controller('toDoCtrl', ['$scope', function ($scope) {
 		});
 		return count;
 	};
+
+	$scope.editOnEnter = function(todo){
+      	$scope.editTask(todo);
+	};
+
+	$scope.editTask = function(todo){
+      todo.editing = !todo.editing;
+	};
+
 	$scope.clearCompleted = function () {
 		$scope.todos = $scope.todos.filter(function (todo) {
 			return !todo.done;
 		});
+		$scope.markAll = false;
 		localStorage.setItem('todos', JSON.stringify($scope.todos));
 	}
+
+	$scope.remaining = function() {
+	    var count = 0;
+	    angular.forEach($scope.todos, function(todo) {
+	      count += todo.done ? 0 : 1;
+	    });
+	    return count;
+	};
+
+	$scope.hasDone = function() {
+      return ($scope.todos.length != $scope.remaining());
+	}
+
+
 	$scope.deleteTask = function(todo) {
 		$scope.todos.splice($scope.todos.indexOf(todo), 1);
 		localStorage.setItem('todos', JSON.stringify($scope.todos));
